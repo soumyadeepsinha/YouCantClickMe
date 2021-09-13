d10d295ec56798815de8e68d8cede8c09f8177e6
soumyadeepsinha <s.sinha2701@gmail.com>
2021-09-13 10:55:45 +0530

    You cant click the button

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is a basic website using HTML, CSS and JavaScript
+
+> The key featre implemented in this website is when user try to click a button, it changes it's position
+
+### Event Listner is used to implement this feature
+
+## With the help of mousemove event, identify the x and y coordinates then change the position
---
+++ app.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('button');
+
+button.style.left = (window.innerWidth / 2 - button.clientWidth / 2) + 'px';
+button.style.top = (window.innerHeight / 2 - button.clientHeight / 2) + 'px';
+
+const padding = 10;
+
+button.addEventListener('click', () => {
+  alert('You Won the Game!');
+})
+
+document.addEventListener('mousemove', ({ clientX: mouseX, clientY: mouseY }) => {
+  const rect = button.getBoundingClientRect();
+  if (mouseX >= rect.x - padding
+    && mouseX <= rect.right + padding
+    && mouseY >= rect.y - padding
+    && mouseY <= rect.bottom + padding) {
+    button.style.left = Math.floor(Math.random() * (window.innerWidth - button.clientWidth)) + 'px';
+    button.style.top = Math.floor(Math.random() * (window.innerHeight - button.clientHeight)) + 'px';
+  }
+});
